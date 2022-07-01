2cb649dfc65e91069b6b9a6d977fb093e456cfea
Danilo <danilo@pop-os.localdomain>
2022-06-30 21:08:22 -0300

    First example

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # example-image-mixer
-Some examples on how to use the image-mixer library.
+Some examples on how to use the [image-mixer](https://www.npmjs.com/package/image-mixer) library.
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { ImageMixer, Layer } from 'image-mixer';
+
+const rootFolder = process.cwd();
+
+const mixer = new ImageMixer({
+  layersPath: `${rootFolder}/assets`,
+  output: `${rootFolder}/output`,
+  layers: [
+    new Layer({ name: 'hair', position: 4, probability: 0.8 }),
+    new Layer({ name: 'eyes_and_noses', position: 3, probability: 1 }),
+    new Layer({ name: 'mouth', position: 2, probability: 1 }),
+    new Layer({ name: 'base', position: 1, probability: 1 }),
+  ],
+});
+
+mixer.generateResults().then((result) => console.log(result));
